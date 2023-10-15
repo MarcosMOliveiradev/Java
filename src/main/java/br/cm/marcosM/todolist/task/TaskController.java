75f70298927be5a066cb02d03e1bd778033da460
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-10-15 17:04:20 -0300

    finalizando

--- src/main/java/br/cm/marcosM/todolist/task/TaskController.java
+++ src/main/java/br/cm/marcosM/todolist/task/TaskController.java
@@ -53,12 +53,22 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
         var task = this.taskRepository.findById(id).orElse(null);
+        var idUser = request.getAttribute("idUser");
 
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Tarefa não encontrada.");
+        }
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuario não tem permissão para modificar essa tarefa.");
+        }
         Utils.copyNonNullPropeties(taskModel, task);
-
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
